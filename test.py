9bbf46961916c84c09c0fb9b3cc71eeffff6fd01
axel agelii <aagelii17@gmail.com>
2022-12-15 14:25:22 -0600

    init

---
+++ test.py
@@ -0,0 +1,40 @@
+import numpy as np # We'll be storing our data as numpy arrays
+import os # For handling directories
+from PIL import Image # For handling the images
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg # Plotting
+
+lookup = dict()
+reverselookup = dict()
+count = 0
+for j in os.listdir('leapGestRecog\\00'):
+    if not j.startswith('.'): # If running this code locally, this is to 
+                              # ensure you aren't reading in hidden folders
+        lookup[j] = count
+        reverselookup[count] = j
+        count = count + 1
+
+x_data = []
+y_data = []
+datacount = 0 # We'll use this to tally how many images are in our dataset
+for i in range(0, 10): # Loop over the ten top-level folders
+    for j in os.listdir('leapGestRecog\\0' + str(i) + '/'):
+        if not j.startswith('.'): # Again avoid hidden folders
+            count = 0 # To tally images of a given gesture
+            for k in os.listdir('leapGestRecog\\0' + 
+                                str(i) + '/' + j + '/'):
+                                # Loop over the images
+                img = Image.open('leapGestRecog\\0' + 
+                                 str(i) + '/' + j + '/' + k).convert('L')
+                                # Read in and convert to greyscale
+                img = img.resize((320, 120))
+                arr = np.array(img)
+                x_data.append(arr) 
+                count = count + 1
+            y_values = np.full((count, 1), lookup[j]) 
+            y_data.append(y_values)
+            datacount = datacount + count
+x_data = np.array(x_data, dtype = 'float32')
+y_data = np.array(y_data)
+y_data = y_data.reshape(datacount, 1) # Reshape to be the correct size
+print(datacount)
